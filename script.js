e309577baf6e3dd1f06bceb61b83e814c6536ac1
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-04-03 04:45:02 -0500

    change testimonial marquee to swiper slider

--- script.js
+++ script.js
@@ -39,6 +39,42 @@ var swiper = new Swiper(".projects-grids-swiper", {
         },
       });
 
+var swiper = new Swiper(".testis-grids-swiper", {
+        slidesPerView: 3,
+        spaceBetween: 25,
+        loop: true,
+        centerSlide: "true",
+        fade: "true",
+        grabCursor: "true",
+        autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          dynamicBullets: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+          },
+        },
+      });
+
 // nav link active state on scroll
 // 1. Target every link inside the .nav-links wrapper
 const navLinks = document.querySelectorAll(".nav-links a");
